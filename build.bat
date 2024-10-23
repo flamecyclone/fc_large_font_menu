dd2a7e385be1d132df50cc1d9789f4ea6152983f
FlameCyclone <1490868135@qq.com>
2024-10-23 10:25:46 +0800

    0

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ set NESASM3=nesasm_Gui_3_2_x86.exe
 
 cd /d %~dp0
 
-FC_Menu_List_Tool.exe -in text/text_title.txt -asm tmp/title_output -list tmp/title_output.txt -line 16 -return 8 -begin 0 -name Title_Text_ -bank_count 1 -index_enable 0
+FC_Menu_List_Tool.exe -in text/text_title.txt -asm tmp/title_output -list tmp/title_output.txt -line 16 -return 8 -begin 0 -name Title_Text_ -bank_count 1 -index_enable 0 -total 16
 FC_Font_Tool_Title.exe -in tmp/title_output.txt -out tmp/chr_title.chr -ini Config/FC_Font_Tool_Title.ini
 FC_Menu_List_Tool.exe -in text/text_list.txt -asm tmp/list_output -list tmp/list_output.txt -line 16 -return 8 -begin 0 -name Item_Text_ -info tmp/list_info.asm -bank_count 8 -index_enable 1
 FC_Font_Tool_List.exe -in tmp/list_output.txt -out tmp/chr_list.chr -ini Config/FC_Font_Tool_List.ini
